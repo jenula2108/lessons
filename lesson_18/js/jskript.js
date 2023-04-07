2217c326fa7612d9db49ff5cd2f2e232ce281c2b
jenula <123267374+jenula2108@users.noreply.github.com>
2023-04-07 19:47:01 +0300

    Delete lesson_18 directory

--- lesson_18/js/jskript.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("click", function (e) {
-// 	const targetElement = e.target;
-
-// 	if (targetElement.closest('.burger__content')) {
-// 		document.documentElement.classList.toggle('burger__navigation');
-// 		e.preventDefault();
-// 	}
-
-// })
