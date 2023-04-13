8eebd1a792f917f572c2047768d28238e0653743
jenula <123267374+jenula2108@users.noreply.github.com>
2023-04-13 21:11:43 +0300

    Add files via upload

---
+++ lesson_19_2/js/jskript.js
@@ -0,0 +1,9 @@
+// document.addEventListener("click", function (e) {
+// 	const targetElement = e.target;
+
+// 	if (targetElement.closest('.burger__content')) {
+// 		document.documentElement.classList.toggle('burger__navigation');
+// 		e.preventDefault();
+// 	}
+
+// })
